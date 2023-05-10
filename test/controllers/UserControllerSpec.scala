2e1d5f935e33958026acd54becda00467c702626
Titouan Guiochet <22703571+PeuTit@users.noreply.github.com>
2023-05-10 10:01:20 +0200

    fix: rewrite test to correct syntax
    Rewrote test files to bracket syntax instead of the indentation based one.
    
    The tests were not compiling with the indent syntax. Scalatest doesn't seem to support that yet.

--- test/controllers/UserControllerSpec.scala
+++ test/controllers/UserControllerSpec.scala
@@ -26,33 +26,36 @@ class UserControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting
     override val actionBuilder: ActionBuilder[Request, AnyContent] = cc.actionBuilder
     override val messagesActionBuilder: MessagesActionBuilder = new DefaultMessagesActionBuilderImpl(parsers.default, messagesApi)(executionContext)
 
-  "UserController GET" should
+  "UserController GET" should {
 
-    "render the index page from a new instance of controller" in
+    "render the index page from a new instance of controller" in {
       val controller = new UserController(new StubComponents())
       val request = FakeRequest().withCSRFToken
       val home = controller.userGet().apply(request)
 
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
-
-    "render the index page from the application" in
+    }
+    "render the index page from the application" in {
       val controller = inject[UserController]
       val request = FakeRequest().withCSRFToken
       val home = controller.userGet().apply(request)
 
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
+    }
 
-    "render the index page from the router" in
+    "render the index page from the router" in {
       val request = CSRFTokenHelper.addCSRFToken(FakeRequest(GET, "/user"))
       val home = route(app, request).get
 
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
+    }
+  }
 
-  "UserController POST" should
-    "process form" in
+  "UserController POST" should {
+    "process form" in {
       val request =
         FakeRequest(POST, "/user")
           .withFormUrlEncodedBody("name" -> "play", "age" -> "4")
@@ -60,4 +63,5 @@ class UserControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting
       val home = route(app, request).get
 
       status(home) mustBe SEE_OTHER
-
+    }
+  }
